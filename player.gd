be44d9aa2b0254b0eb03fd797d1e6b0e1b8622e1
Taran Mittal <taranmittal09@gmail.com>
2025-12-22 22:51:51 +1100

    funny stuff with me messing around

--- player.gd
+++ player.gd
@@ -6,20 +6,17 @@ const JUMP_VELOCITY = -900.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+	var directionx := Input.get_axis("left", "right")
+	if directionx:
+		velocity.x = directionx * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	var directiony := Input.get_axis("up", "down")
+	if directiony:
+		velocity.y = directiony * SPEED
+	else:
+		velocity.y = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
