4e26c312e49ccf91d8f77acf5247cc00b3c1ec8b
Taran Mittal <taranmittal09@gmail.com>
2025-12-23 16:18:34 +1100

    krilling myself

--- player.gd
+++ player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED = 300.0
 const dashSPEED = 900.0
 const JUMP_VELOCITY = -900.0
+var dashLength = 0.2
+var dashCooldown = 1.0
+var lastDash = 2.0
 
 
 func _physics_process(delta: float) -> void:
@@ -22,10 +25,17 @@ func _physics_process(delta: float) -> void:
 	var mouse := get_global_mouse_position()
 	look_at(mouse)
 	
-	var click := Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
-	if click:
-		velocity.y = dashSPEED*sin(rotation)
-		velocity.x = dashSPEED*cos(rotation)
-	
+	dash()		
 	
 	move_and_slide()
+
+
+func dash():
+	var click := Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
+	if click && lastDash > dashCooldown:
+		lastDash = get_tree().create_timer(2)
+		velocity.y = dashSPEED*sin(rotation)
+		velocity.x = dashSPEED*cos(rotation)
+	if lastDash < dashLength:
+			velocity.y = dashSPEED*sin(rotation)
+			velocity.x = dashSPEED*cos(rotation)
