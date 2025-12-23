b0ad86f9caa06f082dbd94d2459a125c53db663d
Taran Mittal <taranmittal09@gmail.com>
2025-12-23 21:27:13 +1100

    enemies that follow the player

--- enemy.gd
+++ enemy.gd
@@ -1,13 +1,17 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
 
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	look_at(Node.Player)
+	look_at(get_node("/root/Node/Player").global_position)
+	var speedY = SPEED*sin(rotation)
+	var speedX = SPEED*cos(rotation)
 	
+	velocity.x = speedX
+	velocity.y = speedY
 
 	move_and_slide()
