c5877157d05142c8369b2b1be7e5ede16562a15f
Taran Mittal <taranmittal09@gmail.com>
2025-12-23 13:05:29 +1100

    added dashing for attacks

--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+const dashSPEED = 900.0
 const JUMP_VELOCITY = -900.0
 
 
@@ -18,7 +19,13 @@ func _physics_process(delta: float) -> void:
 		velocity.y = directiony * SPEED
 	else:
 		velocity.y = move_toward(velocity.x, 0, SPEED)
+	var mouse := get_global_mouse_position()
+	look_at(mouse)
 	
-	look_at(get_global_mouse_position())
-
+	var click := Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
+	if click:
+		velocity.y = dashSPEED*sin(rotation)
+		velocity.x = dashSPEED*cos(rotation)
+	
+	
 	move_and_slide()
