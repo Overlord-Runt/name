4b3c1eeae09e28f2721f7da60606951fb574cf4c
Taran Mittal <taranmittal09@gmail.com>
2025-12-23 16:55:17 +1100

    no longer krilling myself

--- player.gd
+++ player.gd
@@ -4,9 +4,11 @@ extends CharacterBody2D
 const SPEED = 300.0
 const dashSPEED = 900.0
 const JUMP_VELOCITY = -900.0
-var dashLength = 0.2
-var dashCooldown = 1.0
-var lastDash = 2.0
+var canAttack = false
+var dashCooldown = 0.3
+var lastDash = 3.0
+var dashLength = 0.05
+var thisDash = 0.0
 
 
 func _physics_process(delta: float) -> void:
@@ -25,17 +27,28 @@ func _physics_process(delta: float) -> void:
 	var mouse := get_global_mouse_position()
 	look_at(mouse)
 	
-	dash()		
+	dash(delta)		
 	
 	move_and_slide()
 
 
-func dash():
+func dash(delta: float):
 	var click := Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
-	if click && lastDash > dashCooldown:
-		lastDash = get_tree().create_timer(2)
+	lastDash += delta
+	if canAttack:
+		thisDash += delta
+	else:
+		thisDash = 0.0	
+		
+	if lastDash > dashCooldown:
+		if click && thisDash < dashCooldown:
+			canAttack = true
+			thisDash = dashCooldown
+	else:
+		canAttack = false
+	if canAttack:
 		velocity.y = dashSPEED*sin(rotation)
 		velocity.x = dashSPEED*cos(rotation)
-	if lastDash < dashLength:
-			velocity.y = dashSPEED*sin(rotation)
-			velocity.x = dashSPEED*cos(rotation)
+		if thisDash > (dashCooldown + dashLength):
+			lastDash = 0.0
+	
